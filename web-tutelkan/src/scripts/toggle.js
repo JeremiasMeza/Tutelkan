f9a70512e3a7b9c448caa89dafacdf88998ca290
Jeremías Meza Suarez <87507111+JeremiasMeza@users.noreply.github.com>
2025-08-11 09:58:43 -0400

    Allow theme toggle in mobile menu

--- web-tutelkan/src/scripts/toggle.js
+++ web-tutelkan/src/scripts/toggle.js
@@ -1,31 +1,42 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const themeToggle = document.getElementById('theme-toggle');
-  const lightIcon = document.getElementById('theme-toggle-light');
-  const darkIcon = document.getElementById('theme-toggle-dark');
   const html = document.documentElement;
-  const getTheme = () => localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+  const toggles = document.querySelectorAll('.theme-toggle');
+  const getTheme = () =>
+    localStorage.getItem('theme') || (window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light');
+
   const applyTheme = (theme) => {
     if (theme === 'dark') {
       html.classList.add('dark');
-      lightIcon.style.transform = 'rotate(90deg) scale(0)';
-      lightIcon.style.opacity = '0';
-      darkIcon.style.transform = 'rotate(0deg) scale(1)';
-      darkIcon.style.opacity = '1';
     } else {
       html.classList.remove('dark');
-      lightIcon.style.transform = 'rotate(0deg) scale(1)';
-      lightIcon.style.opacity = '1';
-      darkIcon.style.transform = 'rotate(-90deg) scale(0)';
-      darkIcon.style.opacity = '0';
     }
+
+    toggles.forEach((toggle) => {
+      const lightIcon = toggle.querySelector('.theme-toggle-light');
+      const darkIcon = toggle.querySelector('.theme-toggle-dark');
+
+      if (theme === 'dark') {
+        lightIcon.style.transform = 'rotate(90deg) scale(0)';
+        lightIcon.style.opacity = '0';
+        darkIcon.style.transform = 'rotate(0deg) scale(1)';
+        darkIcon.style.opacity = '1';
+      } else {
+        lightIcon.style.transform = 'rotate(0deg) scale(1)';
+        lightIcon.style.opacity = '1';
+        darkIcon.style.transform = 'rotate(-90deg) scale(0)';
+        darkIcon.style.opacity = '0';
+      }
+    });
+
     localStorage.setItem('theme', theme);
   };
+
   applyTheme(getTheme());
-  if (themeToggle) {
-    themeToggle.addEventListener('click', () => {
-      const currentTheme = getTheme();
-      const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+
+  toggles.forEach((toggle) => {
+    toggle.addEventListener('click', () => {
+      const newTheme = getTheme() === 'dark' ? 'light' : 'dark';
       applyTheme(newTheme);
     });
-  }
+  });
 });
